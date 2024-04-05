160bdf7f541b640af2e622d11ce26e82cb1fbe9c
HP-PROBOOK <HP-PROBOOK@CelesteFicoseco>
2024-04-05 19:09:38 -0300

    ejercicio 2

---
+++ tp1_FicosecoCeleste/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pais="Argentina";
+		int edad=30;
+		float h_edificio= (float)45.21;
+		float precio=(float) 350.5 ;
+		long n_telefono=155003366;
+		double coseno = Math.cos(0.5);
+		
+		System.out.println("Pais: "+pais+"\nEdad: "+edad+"\nAltura del edificio:"+h_edificio+"\nPrecio: "+precio
+							+"\nNumero de telefono: "+n_telefono+"\nCoseno(0.5): "+coseno);
+		
+	}
+
+}
