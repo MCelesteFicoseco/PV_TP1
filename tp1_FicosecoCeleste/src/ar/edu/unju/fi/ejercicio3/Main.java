7fc4fe9c705f26451b7cb06e4886592d005cf63b
HP-PROBOOK <HP-PROBOOK@CelesteFicoseco>
2024-04-05 19:26:48 -0300

    ejercicio 3

---
+++ tp1_FicosecoCeleste/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio3;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero entero:");
+		int n = scanner.nextInt();
+		
+		if (Math.floorMod(n, 2) == 0){
+			System.out.println(n*3);
+		}else {
+			System.out.println(n*2);
+		}
+
+	}
+
+}
