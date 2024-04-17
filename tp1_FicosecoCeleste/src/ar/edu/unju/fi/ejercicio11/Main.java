660baa9357561b9b8f54d956a1c1bd39c1195167
HP-PROBOOK <HP-PROBOOK@CelesteFicoseco>
2024-04-16 22:05:44 -0300

    ejercicio 11

---
+++ tp1_FicosecoCeleste/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 2;
+		
+		for(int i = 40; i>0; i--) {
+			System.out.print(i*j+ " ");
+			j++;
+		}
+	}
+
+}
